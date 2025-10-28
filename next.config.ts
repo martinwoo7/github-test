1e919b05a0868ecd6a26725966c483d9c881a3a1
martinwoo7 <28660998+martinwoo7@users.noreply.github.com>
2025-10-28 10:20:46 -0700

    Testing configuation

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // <=== enables static exports
   reactStrictMode: true,
-  basePath: "/github-test",
+  basePath: isProd ? "/github-test" : "",
+  assetPrefix: isProd ? "/github-test/" : "",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
